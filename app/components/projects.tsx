683a643c5f97a1087aba745425569edf367cdc24
Selam Habteab <selamhabteab@Selams-MacBook-Pro.local>
2024-07-30 18:47:31 -0700

    Header part 2 active section indicator

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect } from 'react'
 import SectionHeading from './section-heading';
 import { projectsData } from '../lib/data';
 import Project from './project';
+import { useActiveSectionContext } from '@/context/active-section-context';
+import { useInView } from 'react-intersection-observer';
 
 export default function Projects() {
+  const {ref, inView} = useInView({
+    threshold: 0.5,
+  });
+
+  const { setActiveSection, timeOfLastClick } = useActiveSectionContext()
+ 
+  useEffect(()=> {
+    if (inView && Date.now() - timeOfLastClick > 1000) {
+      setActiveSection("Projects")
+    }
+  }, [inView, timeOfLastClick])
+
   return (
-    <section id="projects" className='scroll-mt-28'>
+    <section ref={ref} id="projects" className='scroll-mt-28'>
         <SectionHeading>My Projects</SectionHeading>
         <div>
             {projectsData.map((projects, index)=> (
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from './components/header'
+import ActiveSectionContextProvider from '@/context/active-section-context'
 //notes: root of app located in layout file.
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,8 +24,10 @@ export default function RootLayout({
       <body className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36`}>
         <div className="bg-red-600 absolute top-[-6rem] -z-10 -right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] "></div>
         <div className="bg-orange-300 absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] 1g:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
-        <Header />
-        {children}
+        <ActiveSectionContextProvider>
+          <Header />
+          {children} 
+        </ActiveSectionContextProvider>
       </body>
     </html>
   )
