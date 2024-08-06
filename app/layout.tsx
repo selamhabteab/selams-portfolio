3f972975ef266c2ca6027f31310587474cc4a81e
Selam Habteab <selamhabteab@Selams-MacBook-Pro.local>
2024-08-06 12:08:08 -0700

    footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import Header from './components/header'
 import ActiveSectionContextProvider from '@/context/active-section-context'
 import { Toaster } from 'react-hot-toast'
+import Footer from './components/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -28,6 +29,7 @@ export default function RootLayout({
         <ActiveSectionContextProvider>
           <Header />
           {children} 
+          <Footer />
           <Toaster position="bottom-center" />
         </ActiveSectionContextProvider>
       </body>
