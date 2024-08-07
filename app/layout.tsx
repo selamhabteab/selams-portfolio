f07a3e77de10e84fe1c5311cd963a670b56f6d8a
Selam Habteab <selamhabteab@Selams-MacBook-Pro.local>
2024-08-06 23:48:53 -0700

    responsiveness fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth">
       <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}>
-        <div className="bg-red-600 absolute top-[-6rem] -z-10 -right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="bg-red-600 absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-orange-300 absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] 1g:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-purple-600"></div>
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
